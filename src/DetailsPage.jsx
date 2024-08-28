2e9207fad2e14d41ba7d75101f9ce6f0647a0e98
yashketanagnihotri <78458048+yashketanagnihotri@users.noreply.github.com>
2024-08-28 21:21:30 +0530

    Added a details component with the order details section

---
+++ src/DetailsPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function DetailsPage() {
+  return (
+    <div className="flow-root rounded-lg border border-gray-100 py-3 shadow-sm">
+      <dl className="-my-3 divide-y divide-gray-100 text-sm">
+        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+          <dt className="font-medium text-gray-900">Title</dt>
+          <dd className="text-gray-700 sm:col-span-2">Mr</dd>
+        </div>
+
+        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+          <dt className="font-medium text-gray-900">Name</dt>
+          <dd className="text-gray-700 sm:col-span-2">John Frusciante</dd>
+        </div>
+
+        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+          <dt className="font-medium text-gray-900">Occupation</dt>
+          <dd className="text-gray-700 sm:col-span-2">Guitarist</dd>
+        </div>
+
+        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+          <dt className="font-medium text-gray-900">Salary</dt>
+          <dd className="text-gray-700 sm:col-span-2">$1,000,000+</dd>
+        </div>
+
+        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+          <dt className="font-medium text-gray-900">Bio</dt>
+          <dd className="text-gray-700 sm:col-span-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis
+            debitis explicabo doloremque impedit nesciunt dolorem facere, dolor
+            quasi veritatis quia fugit aperiam aspernatur neque molestiae labore
+            aliquam soluta architecto?
+          </dd>
+        </div>
+      </dl>
+    </div>
+  );
+}
+
+export default DetailsPage;
